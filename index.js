50f8afcd91c7a2035fe44f17c5f3f32ae0e473b9
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-29 11:59:06 -0400

    commit tree e1829f3d66adffa14dc7e46aa90b0ab7d15cef5b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
